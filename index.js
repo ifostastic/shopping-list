3d1fe671f2a58e9102bc0afd5ecb3c8cb8dec889
Ian Foster <ian.foster@me.com>
2019-12-20 11:20:15 -0500

    finalized functions

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ $("#js-shopping-list-form").submit( e => {
     $('.shopping-list').append("<li><span class='shopping-item'>" + newItem + "</span><div class='shopping-item-controls'><button class='shopping-item-toggle'><span class='button-label'>check</span></button><button class='shopping-item-delete'><span class='button-label'>delete</span></button></div></li>");
 });
 
-$('.shopping-item-toggle').click(function() {
+$('ul').on("click", '.shopping-item-toggle', function() {
     event.preventDefault();
     $(this).closest('.shopping-item-controls').siblings('.shopping-item').toggleClass('shopping-item__checked');
 });
 
-$('.shopping-item-delete').click(function() {
+$('ul').on('click', '.shopping-item-delete', function() {
     event.preventDefault();
     $(this).closest('li').remove();
 });
