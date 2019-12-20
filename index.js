1c74047e0d7fdda7b50d95fc12c3f0f8e737e44d
Ian Foster <ian.foster@me.com>
2019-12-20 10:11:32 -0500

    added base functions to js file

---
+++ index.js
@@ -0,0 +1,15 @@
+$("#js-shopping-list-form").submit( e => {
+    event.preventDefault();
+    let newItem = $("#shopping-list-entry").val();
+    $('.shopping-list').append("<li><span class='shopping-item'>" + newItem + "</span><div class='shopping-item-controls'><button class='shopping-item-toggle'><span class='button-label'>check</span></button><button class='shopping-item-delete'><span class='button-label'>delete</span></button></div></li>");
+});
+
+$('.shopping-item-toggle').click(function() {
+    event.preventDefault();
+    $('.shopping-item').closest('span').toggleClass('shopping-item__checked');
+});
+
+$('.shopping-item-delete').click(function() {
+    event.preventDefault();
+    $('.shopping-item').toggleClass('shopping-item__checked');
+});
