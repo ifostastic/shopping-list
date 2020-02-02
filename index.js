b18aac45d35deeccd863d71b015332e6eff56ce1
Ian Foster <ian.foster@me.com>
2020-02-02 15:54:31 -0500

    added drag to delete functionality

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $("#js-shopping-list-form").submit( e => {
     event.preventDefault();
     let newItem = $("#shopping-list-entry").val();
-    $('.shopping-list').append("<li><span class='shopping-item'>" + newItem + "</span><div class='shopping-item-controls'><button class='shopping-item-toggle'><span class='button-label'>check</span></button><button class='shopping-item-delete'><span class='button-label'>delete</span></button></div></li>");
+    $('.shopping-list').append("<li draggable='true' ondragstart='drag(event)' id='" + newItem + "'><span class='shopping-item'>" + newItem + "</span><div class='shopping-item-controls'><button class='shopping-item-toggle'><span class='button-label'>check</span></button></div></li>");
 });
 
 $('ul').on("click", '.shopping-item-toggle', function() {
@@ -13,3 +13,17 @@ $('ul').on('click', '.shopping-item-delete', function() {
     event.preventDefault();
     $(this).closest('li').remove();
 });
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drag(event) {
+    event.dataTransfer.setData("html", event.target.id);
+}
+
+function drop(event) {
+    let id = event.dataTransfer.getData("html");
+    let elem = document.getElementById(id);
+    elem.remove();
+}
